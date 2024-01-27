3a7e10754cd85dc684ae04d7d7b5b36c512b4d9b
testernjp <nishitjpatel13@gmail.com>
2024-01-27 15:50:21 +0000

    commit3

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("My name is Nishit, how are you?");
         System.out.println("Prime Testing");
+        System.out.println("Manual Testing");
     }
 }

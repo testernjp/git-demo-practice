afedee5ea09fd78495921cce714457e342105de7
testernjp <147002522+testernjp@users.noreply.github.com>
2024-01-27 15:09:38 +0000

    commit3

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("My name is Nishit");
+        System.out.println("My name is Nishit, how are you?");
         System.out.println("Prime Testing");
     }
 }
